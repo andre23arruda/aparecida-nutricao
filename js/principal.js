60de456184beed013c61122232c01e20220a4c21
André Arruda <andre23arruda@gmail.com>
2020-09-26 12:40:53 -0300

    first commit

---
+++ js/principal.js
@@ -0,0 +1,28 @@
+var titulo = document.querySelector(".titulo")
+titulo.textContent = "Aparecida Nutricionista"
+
+/* Calcula IMC */
+var pacientes = document.querySelectorAll('.paciente')
+for(var paciente of pacientes) {
+
+    var imc = ''
+
+    var peso = paciente.querySelector('.info-peso').textContent
+    if (peso <=0 || peso >= 500)
+        imc += ' Peso inválido'
+
+    var altura = paciente.querySelector('.info-altura').textContent
+    if (altura <=0 || altura >= 3)
+        imc += ' Altura inválida'
+
+    if (!(imc)) {
+        imc = peso/(altura ** 2)
+        imc = imc.toFixed(2)
+    }
+    paciente.querySelector('.info-imc').innerText = imc
+
+    // Muda cor se for inválido
+    if (isNaN(imc))
+        // paciente.style.color = 'red' // alterar estilo no js não é adequado
+        paciente.classList.add('paciente-invalido') // adiciona classe no elemento e assim altera estilo
+}
